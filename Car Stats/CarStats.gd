b8fb4c8a38e7fac1a5849b817eef8d2158a6f71c
km2000-a11y <karim.m3_mts@gemsed.com>
2026-05-02 17:12:18 +0400

    dnjcoicon

---
+++ Car Stats/CarStats.gd
@@ -0,0 +1,16 @@
+extends Resource
+class_name CarStats
+
+@export var car_name: String
+@export var engine: String
+@export var drivetrain: String
+@export var horsepower: int
+@export var top_speed: float
+@export var zero_to_hundred: float
+@export var weight: float
+
+@export var max_speed: float
+@export var acceleration: float
+@export var handling: float
+@export var traction: float
+@export var brake_strength: float
