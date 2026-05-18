be293e36b6665cb92416da3644a83aae1183b3dd
km2000-a11y <karim.m3_mts@gemsed.com>
2026-05-18 21:36:44 +0400

    njdxjn dfth kyju

---
+++ Scenes/car_controller.gd
@@ -0,0 +1,19 @@
+extends VehicleBody3D
+
+@export var car_name:String
+@export var country_of_origin:String
+@export var transmission:String
+@export var engine:String
+
+@export var top_speed:int
+@export var horsepower:int
+@export var zero_to_hundred:float
+@export var weight:int
+
+@export var max_speed: float
+@export var acceleration: int
+@export var handling: float
+@export var traction:float
+@export var brake_strength:float
+@export var turn_rate: float
+@export var downforce: float
