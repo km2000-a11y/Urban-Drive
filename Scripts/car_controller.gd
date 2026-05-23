c88c03dc78cbc9689562a64cc1235fc5606f4d6b
km2000-a11y <karim.m3_mts@gemsed.com>
2026-05-23 11:02:48 +0400

    csfvegfb

--- Scripts/car_controller.gd
+++ Scripts/car_controller.gd
@@ -18,18 +18,26 @@ func _physics_process(delta):
 	_apply_braking()
 	_apply_steering()
 	_apply_grip()
+	_apply_stabilizer()
 	_limit_speed()
+	print(accel_input, brake_input, steer_input)
+	print("Forward:", -transform.basis.z)
+
 
 func _get_input():
 	steer_input = Input.get_action_strength("turn_right") - Input.get_action_strength("turn_left")
 	accel_input = Input.get_action_strength("accelerate")
 	brake_input = Input.get_action_strength("brake")
 
+
 func _apply_acceleration():
 	if accel_input > 0.0:
-		var force = -transform.basis.z * accel_force * accel_input
-		apply_force(force, Vector3(0.8, 0, 1))   # front right
-		apply_force(force, Vector3(-0.8, 0, 1))  # front left
+		var force := -transform.basis.z * accel_force * accel_input
+
+		# Apply force EXACTLY at the center → no roll, no tipping
+		apply_central_force(force)
+		print("ACCEL:", accel_input, " FORCE:", -transform.basis.z * accel_force * accel_input)
+
 
 func _apply_braking():
 	if brake_input > 0.0:
@@ -38,12 +46,21 @@ func _apply_braking():
 
 func _apply_steering():
 	var speed = linear_velocity.length()
-	var speed_factor = clamp(speed / 20.0, 0.0, 1.0)
+
+	# Strong steering at low speed, stable at high speed
+	var speed_factor = clamp(speed / 10.0, 0.3, 1.0)
+
 	angular_velocity.y = steer_input * turn_rate * handling * speed_factor
+
 
 func _apply_grip():
 	var lv = transform.basis.inverse() * linear_velocity
 	lv.x *= traction
+	linear_velocity = transform.basis * lv
+
+func _apply_stabilizer():
+	var lv = transform.basis.inverse() * linear_velocity
+	lv.x *= 0.98
 	linear_velocity = transform.basis * lv
 
 func _apply_downforce():
