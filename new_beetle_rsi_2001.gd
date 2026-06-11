4691cb9da67a21c358e78c14b5044758fb2e9b3a
km2000-a11y <karim.m3_mts@gemsed.com>
2026-06-11 20:20:17 +0400

    rtsj

--- new_beetle_rsi_2001.gd
+++ new_beetle_rsi_2001.gd
@@ -5,15 +5,15 @@ var car_name := "Straeda B32"
 var country := "Germany"
 var engine := "V6 3.2L"
 var weight_kg := 1500
-var zero_to_hundred_display := 6.50
+var zero_to_hundred_display := 6.70
 
 func _ready():
 	# GAMEPLAY STATS
 	mass = 1500.0
 	horsepower = 224
 	max_rpm = 6500.0
-	zero_to_hundred = 6.5
-	top_speed_kmh = 234
+	zero_to_hundred = 6.7
+	top_speed_kmh = 229
 	turn_speed = 2.5
 	brake_strength = 20.0
 	lateral_friction = 1.20
